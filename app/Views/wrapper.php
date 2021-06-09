bffaebe27dc395745c7bbee91a0aef06094a0f7e
Prima Nugraha <dotprima@hotmail.com>
2021-06-09 14:31:22 +0700

    add template + ide

--- app/Views/wrapper.php
+++ app/Views/wrapper.php
@@ -10,9 +10,9 @@
     <meta name="keywords"
         content="admin template, Cuba admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="pixelstrap">
-    <link rel="icon" href="../assets/images/favicon.png" type="image/x-icon">
-    <link rel="shortcut icon" href="../assets/images/favicon.png" type="image/x-icon">
-    <title>Cuba - Premium Admin Template</title>
+    <link rel="icon" href="https://primanugraha.tech/public/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="https://primanugraha.tech/public/favicon.ico" type="image/x-icon">
+    <title>Perpustakaan Online Boys</title>
     <!-- Google font-->
     <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&amp;display=swap"
         rel="stylesheet">
